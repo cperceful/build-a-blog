0255f4474bb5227af0ea7d946ff0be0cd5c96321
cperceful <cpercef1@gmail.com>
2017-02-08 17:26:43 -0600

    /blog route and /newpost route and db functionality done, added basic CSS sheet

--- main.py
+++ main.py
@@ -19,10 +19,59 @@ import os;
 import jinja2;
 from google.appengine.ext import db;
 
-class MainHandler(webapp2.RequestHandler):
+templateDir = os.path.join(os.path.dirname(__file__), 'templates');
+jinjaEnv = jinja2.Environment(loader = jinja2.FileSystemLoader(templateDir), autoescape = True);
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw);
+
+    def renderStr(self, template, **params):
+        t = jinjaEnv.get_template(template);
+        return t.render(params);
+
+    def render(self, template, **kw):
+        self.write(self.renderStr(template, **kw));
+
+class Blog(db.Model):
+    title = db.StringProperty(required = True);
+    body = db.TextProperty(required = True);
+    created = db.DateTimeProperty(auto_now_add = True);
+
+class MainHandler(Handler):
+
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect('/blog'); #automatic redirect to /blog
+
+class BlogHandler(Handler):
+
+    def renderFront(self, title = "", body = ""):
+        blogs = db.GqlQuery('SELECT * FROM Blog ORDER BY created DESC LIMIT 5');
+
+        self.render('front.html', title = title, body = body, blogs = blogs);
+
+    def get(self):
+        self.renderFront();
+
+class NewPost(Handler):
+    def get(self):
+        self.render('newpost.html', title = '', body = '', error = '');
+
+    def post(self):
+        title = self.request.get('title');
+        body = self.request.get('body');
+
+        if title and body:
+            blog = Blog(title = title, body = body);
+            blog.put();
+            self.redirect('/blog');
+        else:
+            error = "New posts require a body and a title"
+            self.render('newpost.html', title = title, body = body, error = error);
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', BlogHandler),
+    ('/blog/newpost', NewPost),
 ], debug=True)
